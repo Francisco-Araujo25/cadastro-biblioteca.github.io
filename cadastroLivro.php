d83b0e5eb992200c90feaf02439553edb8f5e3d4
Francisco Araujo <franciscowdearaujo25@gmail.com>
2025-11-21 13:00:25 -0300

    Atualização do Código

--- cadastroLivro.php
+++ cadastroLivro.php
@@ -1,22 +1,18 @@
 <?php 
-//cadastro de livros
+
 $titulo = $_POST['titulo'];
 $autor = $_POST['autor'];
 $ano = $_POST['ano'];
 $editora = $_POST['editora'];
 
 $isbn    = $_POST['isbn'];
-//Livro e cópia
+
 
 $copia = $_POST['valor_copia'];
-$idLivro = $_POST['numero_Id'];
-
-$emprestavel = $_POST['emprestavel']; // valores esperados: 'sim' ou 'nao'
-$tipo_uso = $_POST['tipo_uso']; // valores: 'emprestimo' ou 'consulta'
 
 
-$sala = $_POST['sala_livro'];
-$estante = $_POST['estante_livro'];
+$emprestavel = $_POST['emprestavel']; // 'sim' ou 'nao'
+
 
 echo "<h1>Livro cadastrado!</h1>";
 echo "Título: $titulo <br>";
